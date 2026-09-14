a2e2b6341f95974fc7adec7c3bdfdf8741753f04
Maro Shim <scmaro@gmail.com>
2026-09-14 12:42:46 +0900

    fix(ui): improve status bar message contrast with high-contrast navy background

--- internal/ui/statusbar.go
+++ internal/ui/statusbar.go
@@ -81,8 +81,8 @@ func (sb *StatusBar) Draw(screen tcell.Screen, y, width int) {
 
 	// 2. If there is an active status message within 3 seconds, display it prominently
 	if sb.Message != "" && time.Since(sb.MsgTime) < 3*time.Second {
-		tagStyle := tcell.StyleDefault.Background(tcell.ColorNavy).Foreground(tcell.ColorWhite).Bold(true)
-		msgStyle := tcell.StyleDefault.Background(ColorStatusBarBg).Foreground(tcell.ColorYellow).Bold(true)
+		tagStyle := tcell.StyleDefault.Background(tcell.ColorNavy).Foreground(tcell.ColorYellow).Bold(true)
+		msgStyle := tcell.StyleDefault.Background(tcell.ColorNavy).Foreground(tcell.ColorWhite).Bold(true)
 		tag := " [Turbo] "
 		xPos := 1
 		for _, r := range tag {
